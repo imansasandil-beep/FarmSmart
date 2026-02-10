a8be3429ca7402646d0bd8344bb2aa189fbb1867
Dileesha Dananji <dileesha.20240221@iit.ac.lk>
2026-02-10 09:23:00 +0530

    initialize Post model with basic schema

---
+++ server/models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  authorName: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxLength: 1000,
+  },
+  image: {
+    type: String,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
