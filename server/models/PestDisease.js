9935f399c0037239ec1b6032fb8ed04ceec2c41a
malshichamalka48-ui <malshichamalka48@gmail.com>
2026-02-14 14:15:00 +0530

    feat: add createdAt timestamp to PestDisease schema

--- server/models/PestDisease.js
+++ server/models/PestDisease.js
@@ -22,6 +22,10 @@ const PestDiseaseSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model('PestDisease', PestDiseaseSchema);
