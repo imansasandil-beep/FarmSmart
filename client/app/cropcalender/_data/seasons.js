3dbfff57e0da6eedf2aef978e86bfa1255dbb2a4
sathnidusanupa <sathnidusanupa@gmail.com>
2026-02-19 09:00:00 +0530

    feat: define Yala and Maha season constants

---
+++ client/app/cropcalender/_data/seasons.js
@@ -0,0 +1,31 @@
+﻿// Sri Lanka Cropping Seasons & Monthly Agricultural Advice
+// Sources:
+//   - Dept. of Census & Statistics (http://www.statistics.gov.lk/Agriculture)
+//   - FAO - GIEWS Country Brief Sri Lanka
+//   - Ministry of Agriculture (https://agrimin.gov.lk)
+
+export const SEASONS = {
+    yala: {
+        id: 'yala',
+        name: 'Yala Season',
+        sinhalaName: 'α╢║α╢╜ α╢Üα╢▒α╖èα╢▒α╢║',
+        emoji: 'ΓÿÇ∩╕Å',
+        monsoon: 'Southwest Monsoon',
+        startMonth: 4,  // April
+        endMonth: 8,    // August
+        color: '#f39c12',
+        description: 'Minor cultivation season driven by the Southwest Monsoon. Shorter but intense rainfall, mainly in the south-west.',
+    },
+    maha: {
+        id: 'maha',
+        name: 'Maha Season',
+        sinhalaName: 'α╢╕α╖ä α╢Üα╢▒α╖èα╢▒α╢║',
+        emoji: '≡ƒîº∩╕Å',
+        monsoon: 'Northeast Monsoon',
+        startMonth: 10, // October
+        endMonth: 2,    // February (next year)
+        color: '#2980b9',
+        description: 'Major cultivation season driven by the Northeast Monsoon. Heavier and more continuous rainfall across the island.',
+    },
+};
+
