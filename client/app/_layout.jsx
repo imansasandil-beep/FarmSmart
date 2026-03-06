9e5e2012c7a89b39d9fab6d25e1ae1859f237a61
Imansa Sandil <imansasandil@gmail.com>
2026-03-06 09:30:00 +0530

    feat: add StripeProvider wrapper and marketplace route to root layout

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -1,15 +1,17 @@
 import { Stack } from 'expo-router';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="register" />
-      <Stack.Screen name="success" />
-      <Stack.Screen name="home" />
-      <Stack.Screen name="tasks" />
-      <Stack.Screen name="marketplace" />
-    </Stack>
+    <StripeProvider publishableKey="pk_test_51Sx2NN2ajHmtMsfwhPcB0K2HYT81aAgIaoHgi5Lx3BfzgZPKPGH4uSj3iZIohb59cifyrUZ56LZvCmMVdA5shMxt00uCccirof">
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="success" />
+        <Stack.Screen name="home" />
+        <Stack.Screen name="tasks" />
+        <Stack.Screen name="marketplace" />
+      </Stack>
+    </StripeProvider>
   );
 }
-
